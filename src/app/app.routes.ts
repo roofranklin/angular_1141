74da60bae484d3ee602d4b4b42a964b27304aee5
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-03-20 17:59:20 -0300

    criação das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'quem-somos', component: QuemSomosComponent },
+    { path: 'contato', component: ContatoComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
