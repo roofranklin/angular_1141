226fb43d1cc494014e55ce432c6c2c7caa044f89
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-03-25 17:55:31 -0300

    sombreamento do card ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
