b60c37f6ac4e10e160e9f7e6f12588f818a04768
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-03-26 17:43:07 -0300

    event binding

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [MatSelectModule, MatInputModule, MatFormFieldModule],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.scss'
 })
 export class ContatoComponent {
+    public formMessage!: string;
 
+    public onSubmitForm() {
+        return this.formMessage = 'Formulário enviado com sucesso!';
+    }
 }
