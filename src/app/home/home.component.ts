01e480186e8d53a1adb5215e7cc61d585e761e13
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-03-26 16:24:39 -0300

    card finalizado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatGridListModule, MatButtonModule, MatCardModule],
+  imports: [MatGridListModule, MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
